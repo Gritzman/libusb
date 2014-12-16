6fbba4c812c51d45606816c8799f18342340ecef
Gritzman <Gritzman@users.noreply.github.com>
2014-12-16 12:13:51 +0200

    Create Android.mk

---
+++ jni/usb/Android.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH := $(call my-dir)  
+APP_PLATFORM := android-16
+
+include $(CLEAR_VARS)  
+
+
+LOCAL_SRC_FILES:= \
+	 core.c \
+	 descriptor.c \
+	 io.c \
+	 sync.c \
+	 os/linux_usbfs.c \
+	 os/threads_posix.c 
+			
+LOCAL_LDLIBS := -llog
+
+LOCAL_MODULE := usbapp 
+
+
+include $(BUILD_SHARED_LIBRARY)  
